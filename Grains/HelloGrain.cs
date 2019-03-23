a3f5fdad53fff6195b503ed67c362e1960082190
Carles Company Soler <carles.cs@outlook.com>
2019-03-23 22:57:22 +0100

    Add project files.

---
+++ Grains/HelloGrain.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using GrainInterfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Grains
+{
+    public class HelloGrain:Orleans.Grain,IHello
+    {
+        private readonly ILogger _logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<string> SayHello(string greeting)
+        {
+            _logger.LogInformation($"\n SayHello message received: greeting = '{greeting}'");
+            return Task.FromResult($"\n Client said: '{greeting}', so HelloGrain says: Hello!");
+        }
+    }
+}
---
+++ Tests/HelloGrainTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GrainInterfaces;
+using Microsoft.Extensions.Configuration;
+using Orleans.TestingHost;
+using Xunit;
+
+namespace Tests
+{
+    public class HelloGrainTests
+    {
+
+        [Fact]
+        public async Task SaysHelloCorrectly()
+        {
+            var cluster = new TestCluster(new TestClusterOptions{}, new List<IConfigurationSource>());
+            cluster.Deploy();
+
+            var hello = cluster.GrainFactory.GetGrain<IHello>(0);
+            var greeting = await hello.SayHello("Hola");
+
+            cluster.StopAllSilos();
+
+            Assert.Equal("Hello, World", greeting);
+        }
+    }
+}
